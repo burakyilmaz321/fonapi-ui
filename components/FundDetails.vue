380b354582a4057b07e9862ce45f0c82e2fc677c
Burak Yilmaz <burakyilmaz321@gmail.com>
2021-09-08 23:25:36 +0300

    try adding dynamic content

--- components/FundDetails.vue
+++ components/FundDetails.vue
@@ -3,6 +3,7 @@
 </template>
 
 <script>
+import { createClient } from '@supabase/supabase-js'
 export default {
   data () {
     return {
@@ -11,6 +12,20 @@ export default {
         title: 'TACİRLER ASSET MANAGEMENT VARIABLE FUND'
       }
     }
+  },
+  async fetch () {
+    const supabase = createClient(
+      process.env.SUPABASE_URL,
+      process.env.SUPABASE_KEY
+    )
+    const { data: funds, error } = await supabase
+      .from('funds')
+      .select('*')
+      .eq('code', this.$route.params.code)
+    if (error) {
+      return
+    }
+    this.fund = funds[0]
   }
 }
 </script>
